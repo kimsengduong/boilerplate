11d8729de244f1be2df2bff3a00e0f4777b586e8
duongkimseng <duong.kim.seng@gmail.com>
2022-03-27 15:44:43 +0700

    update install script

--- linux/gitlab/install-arm64.sh
+++ linux/gitlab/install-arm64.sh
@@ -1,3 +1,7 @@
+# create volume directory
 sh ./scripts/create-volume-path.sh
+ 
+# Run Docker Compose
+docker-compose -f docker/docker-compose-arm64.yml up -d
 
-docker stack deploy --compose-file ./docker/docker-compose-arm64.yml gitlab
+# docker stack deploy --compose-file ./docker/docker-compose-arm64.yml gitlab
